8e8ec3c0ad064a7101cdfd4d761d93148d426901
DuyThu <duythutiktok@gmail.com>
2025-03-03 23:09:15 +0700

    update code

--- src/utils/axiosIstance.js
+++ src/utils/axiosIstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://dacs-sany.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
